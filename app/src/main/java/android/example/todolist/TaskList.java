206500886b48083133efd936c106c64734ca841d
Knakano98 <46431690+Knakano98@users.noreply.github.com>
2019-08-29 18:22:45 -0700

    Tweaked delete button to get it 100% correct, added testing functions

--- app/src/main/java/android/example/todolist/TaskList.java
+++ app/src/main/java/android/example/todolist/TaskList.java
@@ -9,10 +9,21 @@ class TaskList {
         taskList.add(task);
     }
 
+    public void removeTask(int index){
+        taskList.remove(index);
+    }
+
     public ArrayList<String> getTaskList(){
         return taskList;
     }
 
+
+    //Test function to print out all tasks
+    public void testList(){
+        for(String item: taskList){
+            System.out.println(item);
+        }
+    }
 
 
 
